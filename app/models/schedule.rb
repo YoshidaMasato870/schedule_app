33fd7ed745a0601bf09c0371109043bc4391f9ac
吉田優斗 <masato@yoshidayuutonoMacBook-Air.local>
2024-07-21 14:28:27 +0900

    validationの質問

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,8 +2,10 @@ class Schedule < ApplicationRecord
     validates :title , presence: true
     validates :start_time ,presence: true
     validates :end_time ,presence: true
-    def validate_past
-        if start_time > end_time
+    validate :validate_past
+      def validate_past
+        unless start_time == nil || end_time =nil
+          self.start_time > self.end_time
         end
-    end
+      end
 end
